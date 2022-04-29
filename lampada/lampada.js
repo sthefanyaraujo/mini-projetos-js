4514b14353076cb8428aa1d990e6b30005937ca9
Sthefany <sthefanyaraujopontes@gmail.com>
2022-04-28 21:24:24 -0300

    :sparkles: Introduce new features.

--- lampada/lampada.js
+++ lampada/lampada.js
@@ -1,5 +1,4 @@
-const turnOn = document.querySelector('#turnOn');
-const turnOff = document.querySelector('#turnOff');
+const turnOnOff = document.querySelector('#turnOnOff');
 const lamp = document.querySelector('#lamp');
 
 
@@ -24,8 +23,17 @@ function lampBroken () {
   lamp.src = './img/quebrada.jpg';
 }
 
-turnOn.addEventListener('click', lampOn);
-turnOff.addEventListener('click', lampOff);
+function lampOnOff () {
+  if(turnOnOff.textContent == 'Ligar') {
+    lampOn();
+    turnOnOff.textContent = 'Desligar';
+  }else{
+    lampOff();
+    turnOnOff.textContent = 'Ligar'
+  }
+}
+
+turnOnOff.addEventListener('click', lampOnOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff)
 lamp.addEventListener('dblclick', lampBroken);
